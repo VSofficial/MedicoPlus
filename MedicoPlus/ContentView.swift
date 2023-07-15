20a4392d5a857b421be45a792510c29689fd0d85
Varun Sharma <vsofficialmail@gmail.com>
2023-07-15 20:32:29 +0530

    Initial Commit

---
+++ MedicoPlus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MedicoPlus
+//
+//  Created by Varun Sharma on 15/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MedicoPlus/MedicoPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicoPlusApp.swift
+//  MedicoPlus
+//
+//  Created by Varun Sharma on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MedicoPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
